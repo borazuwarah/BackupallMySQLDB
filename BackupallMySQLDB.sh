fe75aa948abc92b31796ba72ee42e55f441b7503
Pablo Ramírez <yeyoycareto@gmail.com>
2021-01-26 17:33:45 +0100

    aaç

--- BackupallMySQLDB.sh
+++ BackupallMySQLDB.sh
@@ -1,4 +1,4 @@
-################################
+#################################
 #                               #
 #                               #
 #                               #
@@ -23,17 +23,18 @@ path ='/root/dbBackupTemp'
 
 
 
-# backup DB
-#mkdir -p /root/dbBackupTemp && mysql -uadmin -p`cat /etc/psa/.psa.shadow` -e "show databases;" | grep -v Database | grep -v "\+\-\-" | awk '{print "mysqldump -u admin -p`cat /etc/psa/.psa.shadow` --skip-add-locks "$0" > /root/dbBackupTemp/"$0"$Fecha.sql"}' > /tmp/script.sh && sh /tmp/script.sh 
-#&& tar -czvf /root/dbBackupTemp$Fecha.tar.gz /root/dbBackupTemp/
+ backup DB
+mkdir -p /root/dbBackupTemp && mysql -uadmin -p`cat /etc/psa/.psa.shadow` -e "show databases;" | grep -v Database | grep -v "\+\-\-" | 
+awk '{print "mysqldump -u admin -p`cat /etc/psa/.psa.shadow` --skip-add-locks "$0" > /root/dbBackupTemp/"$0"$Fecha.sql"}' > /tmp/script.sh && sh /tmp/script.sh 
+&& tar -czvf /root/dbBackupTemp$Fecha.tar.gz /root/dbBackupTemp/
 
 
 
 
 #intentamos quitar las BD Schema
-mkdir -p /root/dbBackupTemp && mysql -uadmin -p`cat /etc/psa/.psa.shadow` -e "show databases;" | grep -v Database | grep -v "\+\-\-" | 
-awk '{print "mysqldump -uadmin -p`cat /etc/psa/.psa.shadow` --ignore-database=performance_schema #--ignore-database=information_schema "$0" > /root/dbBackupTemp/"$0"$Fecha.sql"}' > 
-/tmp/script.sh && sh /tmp/script.sh && tar -czvf /root/dbBackupTemp$Fecha.tar.gz /root/dbBackupTemp/*$Fecha.sql
+#mkdir -p /root/dbBackupTemp && mysql -uadmin -p`cat /etc/psa/.psa.shadow` -e "show databases;" | grep -v Database | grep -v "\+\-\-" | 
+#awk '{print "mysqldump -uadmin -p`cat /etc/psa/.psa.shadow` --ignore-database=performance_schema #--ignore-database=information_schema "$0" > /root/dbBackupTemp/"$0"$Fecha.sql"}' > 
+#/tmp/script.sh && sh /tmp/script.sh && tar -czvf /root/dbBackupTemp$Fecha.tar.gz /root/dbBackupTemp/*$Fecha.sql
 
 
 
