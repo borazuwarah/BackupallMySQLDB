74580b0e2ef6fb3f86ceee465ea8a42878212f81
Pablo Ramírez <yeyoycareto@gmail.com>
2021-01-26 17:07:38 +0100

    cambios

--- BackupallMySQLDB.sh
+++ BackupallMySQLDB.sh
@@ -6,7 +6,7 @@
 #                               #
 #                               #
 #                               #
-#                               #           
+#                               #
 #################################
 #                               #
 #                               #
@@ -16,26 +16,24 @@
 Fecha=`date '+%F_%T'`
 path ='/root/dbBackupTemp'
 
-#mkdir -p /root/dbBackupTemp && mysql -uadmin -p`cat /etc/psa/.psa.shadow` -e "show databases;" 
-#| grep -v Database | grep -v "\+\-\-" | awk '{print "mysqldump -                                           
-#uadmin -p`cat /etc/psa/.psa.shadow` "$0" > /root/dbBackupTemp/"$0"$Fecha.sql"}' 
+
+#Backup DB
+#mkdir -p /root/dbBackupTemp && mysql -uadmin -p`cat /etc/psa/.psa.shadow` -e "show databases;"#| grep -v Database | grep -v "\+\-\-" | awk '{print "mysqldump -#uadmin -p`cat /etc/psa/.psa.shadow` "$0" > /root/dbBackupTemp/"$0"$Fecha.sql"}' 
 #> /tmp/script.sh && sh /tmp/script.sh && tar -czvf /root/dbBackupTemp$Fecha.tar.gz /root/dbBackupTemp/*$Fecha.sql
 
 
 
-mkdir -p /root/dbBackupTemp && mysql -uadmin -p`cat /etc/psa/.psa.shadow` -e "show databases;" 
-|grep -v Database | grep -v "\+\-\-" | awk '{print "mysqldump -u admin -p`cat /etc/psa/.psa.shadow` 
---skip-add-locks "$0" > /root/dbBackupTemp/"$0"$Fecha.sql"}' > /tmp/script.sh && sh /tmp/script.sh 
-&& tar -czvf /root/dbBackupTemp$Fecha.tar.gz /root/dbBackupTemp/
+# backup DB
+#mkdir -p /root/dbBackupTemp && mysql -uadmin -p`cat /etc/psa/.psa.shadow` -e "show databases;" | grep -v Database | grep -v "\+\-\-" | awk '{print "mysqldump -u admin -p`cat /etc/psa/.psa.shadow` --skip-add-locks "$0" > /root/dbBackupTemp/"$0"$Fecha.sql"}' > /tmp/script.sh && sh /tmp/script.sh 
+#&& tar -czvf /root/dbBackupTemp$Fecha.tar.gz /root/dbBackupTemp/
 
 
 
 
-#mkdir -p /root/dbBackupTemp && mysql 
-#-uadmin -p`cat /etc/psa/.psa.shadow` -e "show databases;" | grep -v Database | grep -v "\+\-\-" | 
-#awk '{print "mysqldump -uadmin -p`cat /etc/psa/.psa.shadow` --ignore-database=performance_schema 
-#--ignore-database=information_schema "$0" > /root/dbBackupTemp/"$0"$Fecha.sql"}' > 
-#/tmp/script.sh && sh /tmp/script.sh && tar -czvf /root/dbBackupTemp$Fecha.tar.gz /root/dbBackupTemp/*$Fecha.sql
+#intentamos quitar las BD Schema
+mkdir -p /root/dbBackupTemp && mysql -uadmin -p`cat /etc/psa/.psa.shadow` -e "show databases;" | grep -v Database | grep -v "\+\-\-" | 
+awk '{print "mysqldump -uadmin -p`cat /etc/psa/.psa.shadow` --ignore-database=performance_schema #--ignore-database=information_schema "$0" > /root/dbBackupTemp/"$0"$Fecha.sql"}' > 
+/tmp/script.sh && sh /tmp/script.sh && tar -czvf /root/dbBackupTemp$Fecha.tar.gz /root/dbBackupTemp/*$Fecha.sql
 
 
 
